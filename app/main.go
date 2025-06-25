2c8f79a160997d50d6b861170efca71edfb43d8f
t3snake <t3snake@gmail.com>
2025-06-26 01:19:13 +0530

    print errors in stderr

--- app/main.go
+++ app/main.go
@@ -57,6 +57,6 @@ func error(line int, message string) {
 }
 
 func report(line int, where string, message string) {
-	fmt.Printf("[line %d] Error%s: %s\n", line, where, message)
+	fmt.Fprintf(os.Stderr, "[line %d] Error%s: %s\n", line, where, message)
 	had_error = true
 }
